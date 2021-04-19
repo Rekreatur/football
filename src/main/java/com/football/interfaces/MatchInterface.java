760e2921f4f1105fa8e8e6c9a3d29ce0beff746f
Rekreatur <78133876+Rekreatur@users.noreply.github.com>
2021-04-19 17:23:14 +0400

    Expansion Interfaces Methods

--- src/main/java/com/football/interfaces/MatchInterface.java
+++ src/main/java/com/football/interfaces/MatchInterface.java
@@ -1,4 +1,17 @@
 package com.football.interfaces;
 
+import com.football.dto.MatchDto;
+
+import java.util.List;
+
 public interface MatchInterface {
+    List<MatchDto> findAll();
+
+    MatchDto getOneTeam(Long id);
+
+    MatchDto addTeam(MatchDto matchDto);
+
+    MatchDto edit(Long id, MatchDto matchDto);
+
+    void delete(Long id);
 }
--- src/main/java/com/football/interfaces/TournamentInterface.java
+++ src/main/java/com/football/interfaces/TournamentInterface.java
@@ -1,6 +1,5 @@
 package com.football.interfaces;
 
-import com.football.dto.TeamDto;
 import com.football.dto.TournamentDto;
 
 import java.util.List;
@@ -15,5 +14,4 @@ public interface TournamentInterface {
     TournamentDto edit(Long id, TournamentDto tournamentDto);
 
     void delete(Long id);
-
 }
