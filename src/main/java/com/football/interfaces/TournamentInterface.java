0a97f25fec14533961b04bde82e1a86874c6d03a
Rekreatur <78133876+Rekreatur@users.noreply.github.com>
2021-04-19 17:16:10 +0400

    Expansion

--- src/main/java/com/football/interfaces/TournamentInterface.java
+++ src/main/java/com/football/interfaces/TournamentInterface.java
@@ -1,4 +1,18 @@
 package com.football.interfaces;
 
+import com.football.dto.TeamDto;
+import com.football.dto.TournamentDto;
+
+import java.util.List;
+
 public interface TournamentInterface {
+    List<TournamentDto> findAll();
+
+    TournamentDto getOneTeam(Long id);
+
+    TournamentDto addTeam(TournamentDto tournamentDto);
+
+    TournamentDto edit(Long id, TournamentDto tournamentDto);
+
+    void delete(Long id);
 }
